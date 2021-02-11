f6adf2bcfbd1547a28d1165fc7e2f90fa46d5247
pepe <jrpenaseco@gmail.com>
2021-02-11 20:07:32 +0000

    changes in javascript

--- notas/static/javascript/todo.js
+++ notas/static/javascript/todo.js
@@ -1,41 +1,68 @@
 
 window.onload = async function(event){
 
+    /*
+
+    onload():
+
+    Executed when the screen is loaded
+
+
+    */
+
+
+    //==============================
+
+    // SETTING UP ALL HTML ELEMENTS
+
+    //==============================
    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value; 
+   const container = document.getElementById("task-list")
 
-  console.log(csrftoken)
-
+   //making first request to get all todos
   const results = await axios.get("/v1/notas")
 
-  const container = document.getElementById("task-list")
 
-  console.log(results)
-
+  //iterating throw all this todos 
   results.data.map((x) => 
     container.innerHTML += `<h1>${x.content}</h1>`
   )
+
 
   console.log(results)
 
   document.getElementById("add-button").addEventListener("click", async function(){
 
-    const text = document.getElementById("input-task").value
+      const text = document.getElementById("input-task").value
 
-    if(text === "")
-        return
+      if(text === "")
+          return
 
-    const results = await axios.post("/v1/notas/", {user : 1, content : text}, { headers : {'X-CSRFToken' : csrftoken}})
+      const results = await axios.post("/v1/notas/", {user : 1, content : text}, { headers : {'X-CSRFToken' : csrftoken}})
 
-    console.log(results)
+      console.log(results)
 
-    const lastInner = container.innerHTML
+      const lastInner = container.innerHTML
 
-    container.innerHTML = `<h1>${text}</h1>`  + lastInner
+      container.innerHTML = `<h1>${text}</h1>`  + lastInner
 
-    console.log(text)
+      console.log(text)
     
   })
 
 
+}
+
+const renderTodo = (todo) => {
+  const html =  
+
+  `
+
+  hello
+
+
+  `
+
+  return html
 }
 
