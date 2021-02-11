000a8b184d77bb03c6065bbede263159854f926f
Willy <thewillyeb@gmail.com>
2021-02-11 02:55:59 +0100

    css casi hecho pa

--- notas/models.py
+++ notas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 # Create your models here
 
 
 class Nota (models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     content = models.TextField(blank=False,null=False)
     pub = models.DateField(auto_now_add=True)
     hour = models.TimeField(auto_now_add=True)
--- notas/views.py
+++ notas/views.py
@@ -7,6 +7,7 @@ from rest_framework import permissions
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    print("Hello world")
     return render (request,"home.html",{'todos':Nota.objects.all()})
 
 class NotaViewSet (viewsets.ModelViewSet):
