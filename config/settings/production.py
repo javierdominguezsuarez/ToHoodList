64ba4507172c336a0bb8fe9a832f27fbb9e15186
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-02-16 14:01:57 +0000

    settigs

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from config.settings.base import *
 
 DEBUG = False
-
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 # Application definition
